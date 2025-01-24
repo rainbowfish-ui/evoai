06a394e2a40e6cd72360233d7cf975ef52209b43
subhash131 <subhashnayak131@gmail.com>
2025-01-24 20:57:59 +0530

    add hero section

---
+++ src/components/landing/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Hero from "./hero";
+import Navbar from "./navbar";
+import MeetingNotes from "./meeting-notes";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="w-full min-h-screen flex items-center justify-start flex-col px-40">
+        <div className="size-full dashed-vertical-border flex items-center justify-start flex-col">
+          <Hero />
+          <MeetingNotes />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
